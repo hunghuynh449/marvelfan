6160cd284f7d6f11353c58a929eeff4c646a6ae4
Huỳnh Quốc Hưng <49772925+hunghuynh449@users.noreply.github.com>
2022-06-18 12:38:19 +0700

    update api

--- public/javascripts/movies.js
+++ public/javascripts/movies.js
@@ -1,5 +1,5 @@
 const API =
-  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=doraemon";
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=marvel";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 const boxContainer = document.querySelector(".box-container");
